58c9695db87d0405f310d0e995c3189a7f24040c
Alex Objelean <alex.objelean@gmail.com>
2021-12-03 12:02:16 +0200

    update promoCode command implementatin

--- src/main/java/com/ppb/cpp/slack/CPPBotCommand.java
+++ src/main/java/com/ppb/cpp/slack/CPPBotCommand.java
@@ -6,15 +6,21 @@ import com.slack.api.bolt.request.builtin.SlashCommandRequest;
 import com.slack.api.bolt.response.Response;
 import com.slack.api.model.block.element.ButtonElement;
 import com.slack.api.model.block.element.ImageElement;
+import org.jetbrains.annotations.NotNull;
+import org.openapitools.client.model.Promotion;
 import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+import java.util.Optional;
 
 import static com.slack.api.model.block.Blocks.*;
 import static com.slack.api.model.block.composition.BlockCompositions.markdownText;
 import static com.slack.api.model.block.composition.BlockCompositions.plainText;
 import static io.vavr.API.*;
 import static java.lang.String.format;
+import static java.util.Arrays.stream;
 import static org.slf4j.LoggerFactory.getLogger;
 
 /**
@@ -41,8 +47,8 @@ public class CPPBotCommand {
     public Response handle(final SlashCommandRequest req) {
         LOG.info("handling request={}", req.getPayload().getText());
         return Match(new Instruction(req.getPayload())).of(
-                Case($(r -> r.is("promocode")), promoCodeResponse(req)),
-                Case($(r -> r.is("help")), helpResponse(req)),
+                Case($(r -> r.is("promocode")), () -> promoCodeResponse(req)),
+                Case($(r -> r.is("help")), () -> helpResponse(req)),
                 Case($(), unknownInstruction(req)));
     }
 
@@ -51,8 +57,8 @@ public class CPPBotCommand {
     }
 
     private Response helpResponse(final SlashCommandRequest req) {
-        return req.getContext().ack(res -> res.responseType("in_channel")
-                .blocks(asBlocks(
+        return req.getContext().ack(res -> res.responseType("in_channel").blocks(
+                asBlocks(
                         section(section -> section.text(markdownText("*promocode*"))),
                         divider(),
                         section(section -> section.text(markdownText("*help*")))
@@ -60,28 +66,40 @@ public class CPPBotCommand {
     }
 
     private Response promoCodeResponse(final SlashCommandRequest req) {
-        var promo = pphClient.retrievePromotions(req.getPayload().getText().split(" ")[1]);
-        return req.getContext().ack(res -> res.responseType("in_channel")
-            .blocks(asBlocks(
-                section(section -> {
-                    section.text(markdownText("*Name:* " + promo.getName()));
-                    section.accessory(ButtonElement.builder()
-                        .text(plainText("View"))
-                        .value(promo.getTermsAndConditions().getUrl())
-                        .url(promo.getTermsAndConditions().getUrl())
-                        .build());
-                    return section;
-                }),
-                section(section -> {
-                    section.text(markdownText("*Description:* " + promo.getDescription()));
-                    section.accessory(ImageElement.builder()
-                        .imageUrl(promo.getImages().get(4).getUrl())
-                        .altText(promo.getPromoCode())
-                        .build());
-                    return section;
-                }),
-                section(section -> section.text(markdownText("*Brand:* " + promo.getBrand())))
-                )
-            ));
+        return findPromoCode(req)
+                .map(pphClient::retrievePromotions)
+                .map(promo -> responseForPromo(req, promo))
+                .orElseGet(() -> req.getContext().ack(":wave: Unknown promoCode. Try *help* for available instructions.."));
+    }
+
+    private Response responseForPromo(final SlashCommandRequest req, final Promotion promo) {
+        return req.getContext().ack(res -> res.responseType("in_channel").blocks(
+                asBlocks(
+                    section(section -> {
+                        section.text(markdownText("*Name:* " + promo.getName()));
+                        section.accessory(ButtonElement.builder()
+                                .text(plainText("View"))
+                                .value(promo.getTermsAndConditions().getUrl())
+                                .url(promo.getTermsAndConditions().getUrl())
+                                .build());
+                        return section;
+                    }),
+                    section(section -> {
+                        section.text(markdownText("*Description:* " + promo.getDescription()));
+                        section.accessory(ImageElement.builder()
+                                .imageUrl(promo.getImages().get(4).getUrl())
+                                .altText(promo.getPromoCode())
+                                .build());
+                        return section;
+                    }),
+                    section(section -> section.text(markdownText("*Brand:* " + promo.getBrand()))))
+                ));
+    }
+
+    private Optional<String> findPromoCode(final SlashCommandRequest req) {
+        return stream(req.getPayload()
+                .getText()
+                .split("\\w+"))
+                .reduce((first, second) -> second);
     }
 }
